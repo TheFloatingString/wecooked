761dba12f0002351259ed952672ed4f2769151b9
Laurence Liang <laurence.w.liang@gmail.com>
2024-09-15 00:51:35 -0400

    Add more code so this works better for RPi client

--- backend_server/app.py
+++ backend_server/app.py
@@ -5,11 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 import os
+import cohere
+
 
 app = Flask(__name__)
 
 
 GROCERY_LIST = []
+ORIGINAL_GROCERY_LIST = []
 sift = cv2.SIFT_create()
 
 
@@ -17,7 +20,12 @@ def create_prompt(user_resp):
     return f'Rewrite the following as a list of food, all lowercase, no spaces separting but commas separating and only keep food nouns: "{user_resp}"'
 
 
-import cohere
+def return_user_instruction():
+    co = cohere.Client(os.getenv("COHERE_API_KEY"))
+    text_list = co.chat(
+        message="Generate generic grocery store navigation in less than 10 words. Do not generate any newlines."
+    )
+    return text_list.text
 
 
 @app.route("/")
@@ -25,16 +33,32 @@ def hello_world():
     return "hello"
 
 
+@app.route("/api/view/dashboard")
+def api_view_dashboard():
+    global ORIGINAL_GROCERY_LIST
+    global GROCERY_LIST
+    return jsonify(
+        {
+            "original_grocery_list": ORIGINAL_GROCERY_LIST,
+            "live_grocery_list": GROCERY_LIST,
+            "latitude": 43.471759983680,
+            "longitude": -80.53858848369224,
+            "instruction": return_user_instruction(),
+            "img_uri": "https://d6af-129-97-124-163.ngrok-free.app/static/img/generic.jpg",
+        }
+    )
+
+
 @app.route("/api/view/client", methods=["GET", "POST"])
 def api_current():
     global GROCERY_LIST
-    return jsonify({"data": GROCERY_LIST})
+    return jsonify({"grocery_list": GROCERY_LIST})
 
 
 @app.route("/api/view/client/completed_item", methods=["GET", "POST"])
 def api_view_client_completed_item():
     global GROCERY_LIST
-    GROCERY_LIST.pop()
+    GROCERY_LIST.pop(0)
     return jsonify({"data": "substract one"})
 
 
@@ -53,6 +77,7 @@ def sms_reply():
     resp.message(text_list.text)
 
     GROCERY_LIST = list(text_list.text.split(","))
+    ORIGINAL_GROCERY_LIST = GROCERY_LIST.copy()
     print(GROCERY_LIST)
 
     return str(resp)
--- rpi_client/main.py
+++ rpi_client/main.py
@@ -3,37 +3,126 @@ import requests
 import cv2
 import base64
 import numpy as np
+import os
+from dotenv import load_dotenv
 
-# grocery_list = ["coca-cola", "bananas", "orange", "snicker-bar"]
+load_dotenv()
+
+DEBUG = True
 BASE_URL = "https://d6af-129-97-124-163.ngrok-free.app"
+api_key = os.getenv("HTN_OPENAI_API_KEY")
+
+
+def match_count_score(matches: int, n_kp1: int, n_kp2: int) -> float:
+    return 100 * (matches / min(n_kp1, n_kp2))
+
+
+def run_localization(img_query):
+    sift = cv2.SIFT_create()
+    kp_query, des_query = sift.detectAndCompute(img_query, None)
+
+    max_similarity_score = -1
+    most_similar_img = None
+
+    for i in range(100, 108):
+        img_target_query = f"static\\store_phone\\IMG_9{i}.jpg"
+        if DEBUG:
+            print(img_target_query)
+        target_img = cv2.cvtColor(cv2.imread(img_target_query), cv2.COLOR_BGR2RGB)
+        kp_target, des_target = sift.detectAndCompute(target_img, None)
+
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des_query, des_target, k=2)
+
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good.append(m)
+
+        print(
+            match_count_score(
+                matches=len(good), n_kp1=len(kp_query), n_kp2=len(kp_target)
+            )
+        )
+
+
+def is_object_in_image(image_path: str, object_name: str):
+    # Function to encode the image
+    def encode_image(image_path):
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode("utf-8")
+
+    # Getting the base64 string
+    base64_image = encode_image(image_path)
+
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
+
+    payload = {
+        "model": "gpt-4o-mini",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": f"Is there {object_name} this image? Only respond with Yes or No.",
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                    },
+                ],
+            }
+        ],
+        "max_tokens": 300,
+    }
+
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions", headers=headers, json=payload
+    )
+
+    # print(response.json())
+
+    return response.json()["choices"][0]["message"]["content"]
+
 
 while True:
     # perform localization
     time.sleep(1)
     localization_filepath = "static\\localization\\sample_1.jpg"
-    img = cv2.cvtColor(cv2.imread(localization_filepath), cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (256, 256))
-    cv2.imshow("frame", img)
-    cv2.waitKey()
+    img_query = cv2.cvtColor(cv2.imread(localization_filepath), cv2.COLOR_BGR2RGB)
+    img_query = cv2.resize(img_query, (256, 256))
+    run_localization(img_query=img_query)
+
+    # cv2.imshow("frame", img)
+    # cv2.waitKey()
 
     resp = requests.post(f"{BASE_URL}/api/view/client")
     print(resp.json())
-    grocery_list = resp.json()["data"]
+    grocery_list = resp.json()["grocery_list"]
 
     # tap
     input("press enter to tap:")
 
     # detect object of interest
     detection_filepath = "static\\detection\\cocacola.jpg"
-    img = cv2.cvtColor(cv2.imread(detection_filepath), cv2.COLOR_BGR2RGB)
+    img = cv2.imread(detection_filepath)
     img = cv2.resize(img, (256, 256))
-    cv2.imshow("frame", img)
-    cv2.waitKey()
+    cv2.imwrite("tmp.jpg", img)
 
-    # print(grocery_list)
-    # grocery_list.pop()
+    resp = is_object_in_image(image_path="tmp.jpg", object_name=grocery_list[0])
+    print(resp)
+
+    # cv2.imshow("frame", img)
+    # cv2.waitKey()
+
     resp = requests.post(f"{BASE_URL}/api/view/client/completed_item")
-    # print(resp.json())
+    print(resp)
 
     # tap
     input("press enter to tap:")
